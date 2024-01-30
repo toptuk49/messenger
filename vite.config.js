0675c6108ada8be30f2e3aba95969680f494fc7a
toptuk49 <chyka228tigra@yandex.ru>
2024-01-30 11:58:54 +0300

    Removed lightningcss as it does not support SASS

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
-import { browserslistToTargets } from "lightningcss";
-import browserslist from "browserslist";
 import path from "node:path";
 import vitePugPlugin from "vite-plugin-pug-transformer";
 
 export default defineConfig({
-  css: {
-    transformer: "lightningcss",
-    lightningcss: {
-      targets: browserslistToTargets(browserslist(">= 0.25%")),
-    },
-  },
-  build: {
-    cssMinify: "lightningcss",
-  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve("./src") }],
   },
